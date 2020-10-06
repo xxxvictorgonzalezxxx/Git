2f6809facbad460a5bc6fe9cb41cded4e4b2bb67
Victor <victor.gnz10@hotmail.com>
2020-10-06 16:25:58 -0500

    parrafo y tamaño de fuente

---
+++ app.js
@@ -0,0 +1,32 @@
+
+/* Crear un algoritmo q permita determinar la calificacion 
+en letras de un alumno
+
+Si la nota se encuentra entre 100 a 90 = A. 
+Si la nota se enceuntra entre 80 a 89 = B. 
+Si la nota se cncuentra entre 70 a 79 = C.
+Si la nota se encuentra entre 60 a 69 = D. 
+Si la nota es inferior a 60 = F. 
+si dsal s 
+*/
+
+let nota = 70; 
+let notaLetra = '';
+
+if (nota >= 90  ){
+    notaLetra = 'A';
+        
+} else if (nota >= 80 ){
+    notaLetra = 'B';
+} else if (nota >= 70 ){
+    notaLetra = 'C';
+} else if (nota >= 60 ){
+    notaLetra = 'F';   
+} 
+
+console.log(nota + ' es igual a ' + notaLetra);
+ 
+
+
+
+    
